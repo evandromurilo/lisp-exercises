603d8f83e5e90276a4cb9181d8a0650657a6819c
Evandro Murilo <evandro@eficienciafiscal.com.br>
2024-02-25 22:18:17 -0400

    copy norvig_grammar version 2

--- norvig_grammar.lisp
+++ norvig_grammar.lisp
@@ -12,15 +12,34 @@
 (defvar *grammar* *simple-grammar*
   "The grammar used by generate. Initially, this is *simple-grammar*, but we can switch to other grammars.")
 
-(defun generate (rule-name)
-  "Generate a valid sequence for the given rule on the grammar."
-  (let ((rule (cddr (assoc rule-name *grammar*))))
-    (if (listp (car rule))
-        (apply #'append (mapcar #'generate (car rule)))
-        (one-of rule))))
+(defun generate (phrase)
+  "Generate a random sentence of phrase."
+  (cond ((listp phrase)
+         (mappend #'generate phrase))
+        ((rewrites phrase)
+         (generate (random-elt (rewrites phrase))))
+        (t (list phrase))))
+  
+(defun rule-lhs (rule)
+  "The left-hand side of a rule."
+  (first rule))
 
-(defun one-of (lst)
-  "Return one random element of lst as a list."
-  (list (elt lst (random (length lst)))))
+(defun rule-rhs (rule)
+  "The right-hand side of a rule."
+  (rest (rest rule)))
 
-    
+(defun rewrites (category)
+  "Return a list of the possible rewrites for this category."
+  (rule-rhs (assoc category *grammar*)))
+
+(defun one-of (set)
+  "Return one random element of set as a list."
+  (list (random-elt set)))
+
+(defun random-elt (choices)
+  "Choose an element from a list at random."
+  (elt choices (random (length choices))))
+
+(defun mappend (fn the-list)
+  "Apply fn to each element of list and append the results."
+  (apply #'append (mapcar fn the-list)))
