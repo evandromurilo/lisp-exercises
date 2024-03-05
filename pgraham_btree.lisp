3f1381c602da007352df9dbba885b9ec98ba42c1
Evandro Murilo <evandro@eficienciafiscal.com.br>
2024-03-05 13:03:36 -0400

    first version of pgraham_btree.lisp

---
+++ pgraham_btree.lisp
@@ -0,0 +1,11 @@
+(defstruct node obj left right)
+
+(defun bst-insert (node obj order-fn)
+  (let ((new ))
+    (if (funcall order-fn (node-obj node) obj)
+        (if (null (node-left node))
+            (setf (node-left node) (make-node :obj obj))
+            (bst-insert (node-left node) obj order-fn))
+        (if (null (node-right node))
+            (setf (node-right node) (make-node :obj obj))
+            (bst-insert (node-right node) obj order-fn)))))
