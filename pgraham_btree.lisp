113b8e6eb2a7ecaa3603533a0a0ae2622099150f
Evandro Murilo <evandro@eficienciafiscal.com.br>
2024-03-05 13:13:18 -0400

    split into two fns

--- pgraham_btree.lisp
+++ pgraham_btree.lisp
@@ -1,11 +1,14 @@
 (defstruct node obj left right)
 
 (defun bst-insert (obj bst order-fn)
-  (if (funcall order-fn (node-obj bst) obj)
+  (insert-node (make-node :obj obj) bst order-fn))
+
+(defun insert-node (node bst order-fn)
+  (if (funcall order-fn (node-obj bst) (node-obj node))
       (if (null (node-right bst))
-          (setf (node-right bst) (make-node :obj obj))
-          (bst-insert obj (node-right bst) order-fn))
+          (setf (node-right bst) node)
+          (insert-node node (node-right bst) order-fn))
       (if (null (node-left bst))
-          (setf (node-left bst) (make-node :obj obj))
-          (bst-insert obj (node-left bst) order-fn))))
+          (setf (node-left bst) node)
+          (insert-node node (node-left bst) order-fn))))
 
