fbdb4c4093935009d6f36359cac6695686154ee7
Evandro Murilo <evandro@eficienciafiscal.com.br>
2024-03-07 07:00:13 -0400

    review bst-remove

--- pgraham_btree.lisp
+++ pgraham_btree.lisp
@@ -37,28 +37,33 @@
 (defun bst-remove (obj bst <)
   (if (null bst)
       nil
-      (let ((elt (node-elt bst))
-            (l (node-l bst))
-            (r (node-r bst)))
+      (let ((elt (node-elt bst)))
         (if (equal obj elt)
-            (if (null l)
-                r
-                (if (null r)
-                    l
-                    (if (null (node-r l))
-                        (make-node :elt (node-elt l)
-                                   :l (node-l l)
-                                   :r r)
-                        (make-node :elt (node-elt l)
-                                   :l (node-l l)
-                                   :r (make-node
-                                       :elt (node-elt
-                                             (node-r l))
-                                       :r r)))))
-            (if (< obj elt)
+            (percolate bst)
+            (if (funcall < obj elt)
                 (make-node :elt elt
-                           :r r
-                           :l (bst-remove obj l <))
+                           :r (node-r bst)
+                           :l (bst-remove obj (node-l bst) <))
                 (make-node :elt elt
-                           :l l
-                           :r (bst-remove obj r <)))))))
+                           :l (node-l bst)
+                           :r (bst-remove obj (node-r bst) <)))))))
+
+(defun percolate (bst)
+  (cond ((null (node-l bst))
+         (if (null (node-r bst))
+             nil
+             (rperc bst))
+         ((null (node-r)) (lperc bst))
+         (t (if (zerop (random 2))
+                (lperc bst)
+                (rperc bst))))))
+
+(defun rperc (bst)
+  (make-node :elt (node-elt (node-r bst))
+             :l (node-l bst)
+             :r (percolate (node-r bst))))
+
+(defun lperc (bst)
+  (make-node :elt (node-elt (node-l bst))
+             :l (percolate (node-l bst))
+             :r (node-r bst)))
