547265e77306e8ab7251bf126a7521b468a20ad3
Evandro Murilo <evandro@eficienciafiscal.com.br>
2024-03-12 07:53:40 -0400

    add bigger-grammar, generate-tree, generate-all to norvig_grammar

--- norvig_grammar.lisp
+++ norvig_grammar.lisp
@@ -9,6 +9,22 @@
     (Verb -> hit took saw liked))
   "A grammar for a trivial subset of English.")
 
+(defparameter *bigger-grammar*
+  '((sentence -> (noun-phrase verb-phrase))
+    (noun-phrase -> (Article Adj* Noun PP*) (Name) (Pronoun))
+    (verb-phrase -> (Verb noun-phrase PP*))
+    (PP* -> () (PP PP*))
+    (Adj* -> () (Adj Adj*))
+    (PP -> (Prep noun-phrase))
+    (Prep -> to in by with on)
+    (Adj -> big little blue green adiabatic)
+    (Article > the a)
+    (Name -> Pat Kim Lee Terry Robin)
+    (Noun -> man ball woman table)
+    (Verb -> hit took saw liked)
+    (Pronoun -> he she it these those that)))
+     
+
 (defvar *grammar* *simple-grammar*
   "The grammar used by generate. Initially, this is *simple-grammar*, but we can switch to other grammars.")
 
@@ -19,6 +35,41 @@
         ((rewrites phrase)
          (generate (random-elt (rewrites phrase))))
         (t (list phrase))))
+
+(defun generate-tree (phrase)
+  "Generate a random sentence or phrase with a complete parse tree."
+  (cond ((listp phrase)
+         (mapcar #'generate-tree phrase))
+        ((rewrites phrase)
+         (cons phrase
+               (generate-tree (random-elt (rewrites phrase)))))
+        (t (list phrase))))
+
+(defun generate-all (phrase)
+  "Generate a list of all possible expansions of this phrase."
+  (cond ((listp phrase)
+         (outer-permutate (mapcar #'generate-all phrase)))
+        ((rewrites phrase)
+         (mappend #'generate-all (rewrites phrase)))
+        (t (list (list phrase)))))
+
+(defun outer-permutate (lst)
+  (permutate (car lst) (cdr lst)))
+
+(defun permutate (a lst)
+  "Permutates the list of generations a with the lists of generations lst."
+  (cond ((null a) nil)
+        ((null lst) a)
+        (t (append (perm (car a) (permutate (car lst) (cdr lst)))
+              (permutate (cdr a) lst)))))
+         
+(defun perm (a lst)
+  "Permutates the generation a with the list of generations lst."
+  (if (null lst)
+      nil
+      (cons (append a (car lst))
+            (perm a (cdr lst)))))
+         
   
 (defun rule-lhs (rule)
   "The left-hand side of a rule."
