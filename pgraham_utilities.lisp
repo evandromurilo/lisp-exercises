602d04bda9c5552b54983123a8132b9c186fd970
Evandro Murilo <evandro@eficienciafiscal.com.br>
2024-03-20 07:27:29 -0400

    review pgraham_utilities

--- pgraham_utilities.lisp
+++ pgraham_utilities.lisp
@@ -1,22 +1,21 @@
 (defun single? (lst)
-  (and (listp lst)
-       (car lst)
-       (null (cdr lst))))
+  (and (consp lst) (null (cdr lst))))
 
-(defun append1 (lst elt)
-  (append lst (list elt)))
+(defun append1 (lst obj)
+  (append lst (list obj)))
 
 (defun map-int (fn n)
   (let ((acc nil))
-    (dotimes (i n (reverse acc))
-      (push (funcall fn i) acc))))
-
-(defun filter (predicate lst)
-  (let ((acc nil))
-    (dolist (elt lst)
-      (when (funcall predicate elt)
-        (push elt acc)))
+    (dotimes (i n)
+      (push (funcall fn i) acc))
     (reverse acc)))
+
+(defun filter (fn lst)
+  (let ((acc nil))
+    (dolist (x lst)
+      (let ((val (funcall fn x)))
+        (if val (push val acc))))
+    (nreverse acc)))
 
           
 (defun most (scorer lst)
@@ -29,4 +28,16 @@
                        (most-rec score elt (cdr lst)))))))
     (most-rec 0 nil lst)))
 
+(defun most (fn lst)
+  (if (null lst)
+      (values nil nil)
+      (let* ((wins (car lst))
+             (max (funcall fn wins)))
+        (dolist (obj (cdr lst))
+          (let ((score (funcall fn obj)))
+            (when (> score max)
+              (setf wins obj
+                    max score))))
+        (values wins max))))
+                
   
