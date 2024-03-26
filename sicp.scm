c76c14d5b6b48cdad843720a3b8495951c3a1733
Evandro Murilo <evandro@eficienciafiscal.com.br>
2024-03-26 13:35:12 -0400

    working on section 1.3.1 exercises

--- sicp.scm
+++ sicp.scm
@@ -81,3 +81,114 @@
         ((= n 1) (* b extra))
         ((even? n) (exp-iter (square b) (/ n 2) extra))
         (else (exp-iter (square b) (/ (- n 1) 2) (* b extra)))))
+
+;; 1.17 Using addition, double, and halve, design a multiplication procedure analogous to fast-expt that uses a logarithmic number of steps.
+
+(define (double n)
+  (+ n n))
+
+(define (halve n)
+  (/ n 2))
+
+(define (times a b)
+  (cond ((= b 0) 0)
+        ((even? b) (times (double a) (halve b)))
+        (else (+ a (times (double a) (halve (- b 1)))))))
+  
+;; 1.18 Using the results of exercises 1.16 and 1.17, devise a procedure that generates an iterative process for multiplying two integers in terms of adding, doubling and halving and uses a logarithmic number of steps.
+
+(define (times a b)
+  (times-iter a b 0))
+
+(define (times-iter a b extra)
+  (cond ((= b 0) (+ 0 extra))
+        ((even? b) (times-iter (double a) (halve b) extra))
+        (else (times-iter (double a) (halve (- b 1)) (+ extra a)))))
+
+;; 1.30 The sum procedure above generates a linear recursion. The procedure can be rewritten so that the sum is performed iteratively. Show how to do this by filling in the missing expressions in the following definition:
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+;; 1.31 a. The sum procedure is only the simplest of a vast number of similar abstractions that can be captured as higher-order procedures. Write an analogous procedure called product that returns the product of the values of a function at points over a given range. Show how to define factorial in termos of product.
+
+(define (identity a) a)
+
+(define (inc a) (+ a 1))
+
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (term a) (product term
+                           (next a)
+                           next
+                           b))))
+
+(define (factorial n)
+  (product identity 1 inc n))
+
+;; 1.31 b. If your product procedure generates a recursive process, write one that generates an iterative process. If it genrates an iterative process, write one that generates a recursive process.
+
+(define (product term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+;; 1.32 a. Show that sum and product are both special cases of a still more general notion called accumulate that combines a collection of terms, using some general accumulation function:
+;; (accumulate combiner null-value term a next b)
+;; Write accumulate and show how sum and product can both be defined as simple calls to accumulate.
+
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner
+                            null-value
+                            term
+                            (next a)
+                            next
+                            b))))
+
+(define (product term a next b)
+  (accumulate * 1 term a next b))
+
+(define (sum term a next b)
+  (accumulate + 0 term a next b))
+
+;; 1.32 b. If your accumulate procedure genrates a recursive process, write one that generates an iterative process. If it generates an iterative process, write one that generates a recursive process.
+
+(define (accumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner (term a) result))))
+  (iter a null-value))
+
+;; 1.33 You can obtain an even more general version of accumulate by introducing the notion of a filter on the terms to be combined. That is, combine only those terms derived from values in the range that satisfy a specified condition. The resulting filtered-accumulate abstraction take the same arguments as accumulate, together with an additional predicate of one argument that specifies the filter. Write filtered accumulate as a procedure.
+
+(define (filtered-accumulate combiner null-value filter term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (if (filter a)
+            (iter (next a) (combiner (term a) result))
+            (iter (next a) result))))
+  (iter a null-value))
+
+(define (sum-even n)
+  (filtered-accumulate + 0 even? identity 1 inc n))
+
+;; 1.33 a. Show how to express the sum of the squares of prime numbers in the interval a to b (assuming you have a prime? predicate already written).
+
+(define (sum-square-of-primes a b)
+  (filtered-accumulate + 0 prime? square a inc b))
+
+
+;; 1.33 b. Show how to express the product of all the positive integers less than n that are relatively prime to n (i.e., all positive integers i < n such that GCD(i, n) = 1)
+
