018246dbd9d558a6836965cac76069b42d9ce6a8
Evandro Murilo <evandro@eficienciafiscal.com.br>
2024-04-08 17:32:18 -0400

    save assoc on table

--- pgraham_random_text.lisp
+++ pgraham_random_text.lisp
@@ -1,6 +1,10 @@
 (setf word-table (make-hash-table :test #'equal))
 
+(defun reset-table ()
+  (setf word-table (make-hash-table :test #'equal)))
+
 (defun read-text (path)
+  (reset-table)
   (with-open-file (str path)
     (let ((word (make-string 100))
           (i 0))
@@ -13,10 +17,19 @@
             (progn
               (setf (char word i) ch)
               (incf i))))))
+  (show-table))
+
+(defun show-table ()
   (maphash #'(lambda (k v) (format t "~A: ~A~%" k v)) word-table))
 
-(defun add-word (word)
-  (incf (gethash word word-table 0)))
+(defun add-word (word prev)
+  (let ((word-assoc (gethash word word-table)))
+    (if (null word-assoc)
+        (setf (gethash word word-table) (list (cons prev 1)))
+        (let ((pair (assoc prev word-assoc)))
+          (if (null pair)
+              (setf (gethash word word-table) (push (cons prev 1) word-assoc))
+              (incf (cdr pair)))))))
 
 (defun punc-p (ch)
   (case ch ((#\  #\. #\, #\! #\?) t)
